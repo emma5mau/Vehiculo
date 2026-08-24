0f9e3a71aab2569e3704c22bb4c7ebf4c20719d2
Emmanuel <165523239+emma5mau@users.noreply.github.com>
2026-08-24 09:42:08 -0500

    feature: clase-base

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -2,7 +2,7 @@ public class Vehiculo {
     private String marca;
     private String modelo;
     private String color;
-    private int anyo;
+    private int año;
     private int velocidad;
     private boolean encendido;
 
