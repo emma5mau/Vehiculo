022ace4975d51a18bfdd79e414fd7584045d0a38
Esparza <santiesparza0722@gmail.com>
2026-08-26 21:32:11 -0500

    Agregado metodo frenar

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -6,5 +6,15 @@ public class Vehiculo {
     private int velocidad;
     private boolean encendido;
 
-
+    public void frenar(int decremento) {
+        if (encendido) {
+            velocidad -= decremento;
+            if (velocidad < 0) {
+                velocidad = 0;
+            }
+            System.out.println("Has frenado. Velocidad actual: " + velocidad + " km/h");
+        } else {
+            System.out.println("El vehículo está apagado.");
+        }
+    }
 }
